f75d842f26568c5c456dbe33d71fe4889b1adec7
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-09-19 22:23:47 +0530

    Done

--- Session-9/my-app/src/App.js
+++ Session-9/my-app/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Container} from 'react-bootstrap';
+import {Button,Row,Col} from 'react-bootstrap';
 
 function App() {
   return (
@@ -10,8 +10,18 @@ function App() {
       <Button variant="danger" style={{border:"2px solid blue"}} size="sm" disabled>Submit</Button>
       <Button variant="dark" href="https://www.google.com/" size="lg" >Google</Button>
       <Button variant="link" href="https://www.facebook.com/">Facebook</Button>
+
+      <div className='division'>
+        <Row className='row'>
+          <Col className='col1' lg={3}>UPI (Gpay/PayTm/PhonePe)</Col>
+          <Col className='col2' lg={3}>Credit/Debit Card</Col>
+          <Col className='col3' lg={3}>Net Banking</Col>
+          <Col className='col4' lg={3}>Other Payments</Col>
+        </Row>
+      </div>
+
     </div>
-  );
+  ); 
 }
 
 export default App;
