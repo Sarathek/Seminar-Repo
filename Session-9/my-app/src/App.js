c40e15feadacfa69910c60872b2a9d03dddab4d8
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-09-19 17:00:48 +0530

    Ongoing

---
+++ Session-9/my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Container} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Payment App</h1>
+      <Button variant="success">Select</Button>
+      <Button variant="danger" style={{border:"2px solid blue"}} size="sm" disabled>Submit</Button>
+      <Button variant="dark" href="https://www.google.com/" size="lg" >Google</Button>
+      <Button variant="link" href="https://www.facebook.com/">Facebook</Button>
+    </div>
+  );
+}
+<div>
+
+export default App;
