3c8b01ec7094403fb081eb6560045a0dda2f81c5
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-09-21 16:13:48 +0530

    Started

---
+++ Session-10/styled-compo/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+
+  const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+return (
+  <div className="App">
+render(
+  <Wrapper>
+    <Title>
+      Hello World!
+    </Title>
+  </Wrapper>
+);
+    </div>
+  );
+}
+
+export default App;
