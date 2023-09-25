2198c420f770dd4e49a6a20c6b2adaa5951ce923
Sarathek <135970994+Sarathek@users.noreply.github.com>
2023-09-25 06:45:18 +0530

    Changes

--- Session-10/styled-compo/src/App.js
+++ Session-10/styled-compo/src/App.js
@@ -7,10 +7,8 @@ import Button from './button';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Styled Components Example</h1>
         <Button onClick={() => alert('Button clicked')}>Click Me</Button>
-      </header>
     </div>
   );
 }
